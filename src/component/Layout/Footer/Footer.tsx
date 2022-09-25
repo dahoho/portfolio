a14763d9d1c25fc666648ffa6d6d2934f2d39a17
hoda <markernet.hoda@gmail.com>
2022-09-25 23:56:09 +0900

    🔥 フッターリンク削除

--- src/component/Layout/Footer/Footer.tsx
+++ src/component/Layout/Footer/Footer.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 import { Container } from "../Container";
 import styles from "./index.module.scss";
 
@@ -7,60 +5,6 @@ export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <Container>
-        <nav>
-          <ul className={styles.list}>
-            <li>
-              <Link href="/">
-                <a className={styles.navLink}>Top</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/about">
-                <a className={styles.navLink}>About</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/works">
-                <a className={styles.navLink}>Works</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/timeline">
-                <a className={styles.navLink}>Timeline</a>
-              </Link>
-            </li>
-            <li>
-              <a
-                href="https://zenn.dev/rh820"
-                className={styles.navLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Zenn
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://twitter.com/@RHweb0820"
-                className={styles.navLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Twitter
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/dahoho"
-                className={styles.navLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Github
-              </a>
-            </li>
-          </ul>
-        </nav>
         <p className={styles.copy}>
           <small lang="en" translate="no">
             &copy; 2022 dahoho inc.
