dfacdc183b458eee0bf1e24115b03d4c924a07c2
hoda <markernet.hoda@gmail.com>
2022-10-08 08:53:19 +0900

    🩹 ヘッダー調整

--- src/component/Layout/Footer/Footer.tsx
+++ src/component/Layout/Footer/Footer.tsx
@@ -1,16 +1,13 @@
-import { Container } from "../Container";
 import styles from "./index.module.scss";
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Container>
-        <p className={styles.copy}>
-          <small lang="en" translate="no">
-            &copy; 2022 dahoho inc.
-          </small>
-        </p>
-      </Container>
+      <p className={styles.copy}>
+        <small lang="en" translate="no">
+          &copy; 2022 dahoho inc.
+        </small>
+      </p>
     </footer>
   );
 };
--- src/component/Layout/Header/Header.tsx
+++ src/component/Layout/Header/Header.tsx
@@ -1,22 +1,19 @@
 import Link from "next/link";
 
-import { Container } from "../Container";
 import { Menu } from "../Menu";
 import styles from "./index.module.scss";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <Container>
-        <div className={styles.headerIn}>
-          <Menu />
-          <h1 className={styles.title}>
-            <Link href="/">
-              <a>dahoho</a>
-            </Link>
-          </h1>
-        </div>
-      </Container>
+      <div className={styles.headerIn}>
+        <Menu />
+        <h1 className={styles.title}>
+          <Link href="/">
+            <a>dahoho</a>
+          </Link>
+        </h1>
+      </div>
     </header>
   );
 };
