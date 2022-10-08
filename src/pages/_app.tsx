9c8988a7879467c96ab817cc60b64717a574e191
hoda <markernet.hoda@gmail.com>
2022-10-08 16:05:08 +0900

    GA導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,11 @@ import "src/styles/normalize.scss";
 import "src/styles/globals.scss";
 
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Script from "next/script";
 import { DefaultSeo } from "next-seo";
+import { useEffect } from "react";
+import * as gtag from "src/lib/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // meta関連
@@ -10,8 +14,37 @@ function MyApp({ Component, pageProps }: AppProps) {
     title: "dahoho Portfolio site",
     description: "dahohoのポートフォリオサイトです。",
   };
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
       <DefaultSeo
         defaultTitle={siteInfo.title}
         description={siteInfo.description}
