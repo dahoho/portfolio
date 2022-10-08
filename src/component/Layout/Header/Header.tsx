7046a2ea45432645ae553d9c3c59a172a1a64edd
hoda <markernet.hoda@gmail.com>
2022-10-08 15:04:56 +0900

    ✨ ダークモード・ライトモード切り替え実装

--- src/component/Layout/Header/Header.tsx
+++ src/component/Layout/Header/Header.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 import { Menu } from "../Menu";
 import styles from "./index.module.scss";
 
 export const Header = () => {
+  const [lightTheme, setLightTheme] = useState("");
+
+  const handleToggle = (e: any) => {
+    setLightTheme(e.target.checked);
+  };
+
+  useEffect(() => {
+    if (lightTheme !== "") {
+      if (lightTheme) {
+        document.documentElement.setAttribute("data-theme", "light");
+        window.localStorage.setItem("theme", "light");
+      } else {
+        document.documentElement.removeAttribute("data-theme");
+        window.localStorage.setItem("theme", "dark");
+      }
+    }
+  }, [lightTheme]);
+
   return (
     <header className={styles.header}>
       <div className={styles.headerIn}>
@@ -15,7 +34,11 @@ export const Header = () => {
         </h1>
         <form action="#" className={styles.toggle}>
           <label className={styles.switch}>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              onChange={handleToggle}
+              checked={lightTheme}
+            />
             <span className={styles.slider}></span>
           </label>
         </form>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,12 +14,53 @@ class Document extends NextDocument<Props> {
           ></link>
         </Head>
         <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: themeInitializerScript,
+            }}
+          ></script>
           <Main />
           <NextScript />
         </body>
       </Html>
     );
   }
+}
+
+const themeInitializerScript = `(function() {
+	${setInitialColorMode.toString()}
+	setInitialColorMode();
+})()
+`;
+
+function setInitialColorMode() {
+  // 最初のpreferenceを確認して、darkかlightの文字列を返す関数
+  function getInitialColorMode() {
+    //ストレージからthemeは取得する。
+    const persistedPreferenceMode = window.localStorage.getItem("theme");
+    const hasPersistedPreference = typeof persistedPreferenceMode === "string";
+
+    if (hasPersistedPreference) {
+      return persistedPreferenceMode;
+    }
+
+    const preference = window.matchMedia("(prefers-color-scheme: light)");
+    const hasMediaQueryPreference = typeof preference.matches === "boolean";
+
+    if (hasMediaQueryPreference) {
+      return preference.matches ? "light" : "dark";
+    }
+
+    return "dark";
+  }
+
+  const currentColorMode = getInitialColorMode();
+  const element = document.documentElement;
+
+  element.style.setProperty("--initial-color-mode", currentColorMode);
+
+  if (currentColorMode === "light")
+    document.documentElement.setAttribute("data-theme", "light");
 }
 
 export default Document;
