94f6895b48821426ffa0245db3ca614c9acbf8d8
hoda <markernet.hoda@gmail.com>
2022-10-09 17:08:51 +0900

    ✨ ブログ記事を追加

--- src/component/Blog/Blog.tsx
+++ src/component/Blog/Blog.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs";
+import Link from "next/link";
+
 import { HeadlineLg } from "../Headline/HeadlineLg";
 import { Container } from "../Layout/Container";
 import { Spacer } from "../Spacer";
@@ -10,7 +13,7 @@ export const Blog = ({ blogdata }: any) => {
         <HeadlineLg title="Blog" />
         <Spacer size="lg" />
         <section>
-          {/* <ul>
+          <ul>
             {blogdata.contents.map((item: any) => {
               return (
                 <li key={item.id} className={styles.item}>
@@ -25,8 +28,8 @@ export const Blog = ({ blogdata }: any) => {
                 </li>
               );
             })}
-          </ul> */}
-          <p className={styles.text}>comming soon</p>
+          </ul>
+          {/* <p className={styles.text}>comming soon</p> */}
         </section>
       </Container>
     </>
