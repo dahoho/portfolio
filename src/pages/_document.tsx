0fed27afc1093792d30419986be6cddd3f883664
hoda <markernet.hoda@gmail.com>
2022-11-06 11:05:50 +0900

    🔥 不要な記述を削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -59,34 +59,4 @@ function setInitialColorMode() {
   }
 }
 
-// function setInitialColorMode() {
-//   // 最初のpreferenceを確認して、darkかlightの文字列を返す関数
-//   function getInitialColorMode() {
-//     //ストレージからthemeは取得する。
-//     const persistedPreferenceMode = window.localStorage.getItem("theme");
-//     const hasPersistedPreference = typeof persistedPreferenceMode === "string";
-
-//     if (hasPersistedPreference) {
-//       return persistedPreferenceMode;
-//     }
-
-//     const preference = window.matchMedia("(prefers-color-scheme: light)");
-//     const hasMediaQueryPreference = typeof preference.matches === "boolean";
-
-//     if (hasMediaQueryPreference) {
-//       return preference.matches ? "light" : "dark";
-//     }
-
-//     return "dark";
-//   }
-
-//   const currentColorMode = getInitialColorMode();
-//   const element = document.documentElement;
-
-//   element.style.setProperty("--initial-color-mode", currentColorMode);
-
-//   if (currentColorMode === "light")
-//     document.documentElement.setAttribute("data-theme", "light");
-// }
-
 export default Document;
