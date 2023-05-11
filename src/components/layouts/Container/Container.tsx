ec040d93900a429861f61939710b7738132ccb79
hoda <h.ryosuke820@gmail.com>
2023-05-11 23:04:08 +0900

    不要な記述を削除

--- src/components/layouts/Container/Container.tsx
+++ src/components/layouts/Container/Container.tsx
@@ -3,9 +3,5 @@ import styles from "src/components/layouts/Container/index.module.scss";
 import { ChildrenProps } from "@/types";
 
 export const Container = ({ children }: ChildrenProps): JSX.Element => {
-  return (
-    <>
-      <div className={styles.container}>{children}</div>
-    </>
-  );
+  return <div className={styles.container}>{children}</div>;
 };
--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -9,33 +9,31 @@ import { GITHUB_URL, NOTE_URL, TWITTER_URL } from "@/constants";
 
 export const Card = () => {
   return (
-    <>
-      <ul className={styles.list}>
-        <li className={`${styles.item} ${styles.about}`}>
-          <Link href="about/">About</Link>
-        </li>
-        <li className={`${styles.item} ${styles.blog}`}>
-          <Link href="blog/">Tech Blog</Link>
-        </li>
-        <li className={`${styles.item} ${styles.github}`}>
-          <Link href={GITHUB_URL} target="_blank">
-            <Image src={logoGithub} alt="" priority={true} />
-          </Link>
-        </li>
-        <li className={`${styles.item} ${styles.twitter}`}>
-          <Link href={TWITTER_URL} target="_blank">
-            <Image src={logoTwitter} alt="" priority={true} />
-          </Link>
-        </li>
-        <li className={`${styles.item} ${styles.note}`}>
-          <Link href={NOTE_URL} target="_blank">
-            <Image src={logoNote} alt="" priority={true} />
-          </Link>
-        </li>
-        <li className={`${styles.item} ${styles.work}`}>
-          <Link href="works/">works</Link>
-        </li>
-      </ul>
-    </>
+    <ul className={styles.list}>
+      <li className={`${styles.item} ${styles.about}`}>
+        <Link href="about/">About</Link>
+      </li>
+      <li className={`${styles.item} ${styles.blog}`}>
+        <Link href="blog/">Tech Blog</Link>
+      </li>
+      <li className={`${styles.item} ${styles.github}`}>
+        <Link href={GITHUB_URL} target="_blank">
+          <Image src={logoGithub} alt="" priority={true} />
+        </Link>
+      </li>
+      <li className={`${styles.item} ${styles.twitter}`}>
+        <Link href={TWITTER_URL} target="_blank">
+          <Image src={logoTwitter} alt="" priority={true} />
+        </Link>
+      </li>
+      <li className={`${styles.item} ${styles.note}`}>
+        <Link href={NOTE_URL} target="_blank">
+          <Image src={logoNote} alt="" priority={true} />
+        </Link>
+      </li>
+      <li className={`${styles.item} ${styles.work}`}>
+        <Link href="works/">works</Link>
+      </li>
+    </ul>
   );
 };
