f1752c4b4f5ebfaa1ff3b6bdb9c81dca27b6c4ce
hoda <h.ryosuke820@gmail.com>
2023-09-04 14:45:48 +0900

    googleフォント設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import "@/styles/globals.scss";
 
+import { Noto_Sans_JP } from "next/font/google";
+
+const notojp = Noto_Sans_JP({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className={notojp.className}>{children}</body>
     </html>
   );
 };
