015d873bb031e05c44e023e4b00211dbf177cd5e
hoda <h.ryosuke820@gmail.com>
2025-01-13 12:24:01 +0900

    HeadingLargeコンポーネントのスタイルを変更

--- src/app/components/ui/heading/HeadingLarge.tsx
+++ src/app/components/ui/heading/HeadingLarge.tsx
@@ -1,5 +1,5 @@
 import { ChildrenType } from '@/app/types/children'
 
 export const HeadingLarge = ({ children }: ChildrenType) => {
-  return <h2 className="text-3xl">{children}</h2>
+  return <h2 className="text-3xl text-title">{children}</h2>
 }
