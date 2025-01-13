0acce3fda37dd70ef6591a12ac7434fa1bb5a4d2
hoda <h.ryosuke820@gmail.com>
2025-01-13 12:23:33 +0900

    Productコンポーネントを追加

---
+++ src/app/components/product/Product.tsx
@@ -0,0 +1,14 @@
+import { InnerLayout } from '@/app/components/layout/inner'
+import { Section } from '@/app/components/layout/section'
+import { HeadingLarge } from '@/app/components/ui/heading'
+
+export const Product = () => {
+  return (
+    <Section>
+      <HeadingLarge>Product</HeadingLarge>
+      <InnerLayout>
+        <p>Coming soon...</p>
+      </InnerLayout>
+    </Section>
+  )
+}
