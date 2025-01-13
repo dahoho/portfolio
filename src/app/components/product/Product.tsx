7cb0b9614022b7f977ac61249e31aa2d578e45c2
hoda <h.ryosuke820@gmail.com>
2025-01-13 14:00:54 +0900

    Productコンポーネントに制作物を追加

--- src/app/components/product/Product.tsx
+++ src/app/components/product/Product.tsx
@@ -7,7 +7,24 @@ export const Product = () => {
     <Section>
       <HeadingLarge>Product</HeadingLarge>
       <InnerLayout>
-        <p>Coming soon...</p>
+        <ul className="flex flex-col gap-4">
+          <li className="bg-card rounded-md">
+            <a
+              href="https://reart-renovation.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="block p-4"
+            >
+              <div className="flex items-center gap-2">
+                <span className="text-xs bg-accent text-white p-1 rounded-sm">
+                  WEBサイト
+                </span>
+                <p>株式会社リアート様</p>
+              </div>
+              <p className="mt-2">https://reart-renovation.com/</p>
+            </a>
+          </li>
+        </ul>
       </InnerLayout>
     </Section>
   )
