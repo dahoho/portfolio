25c5c1f134eaada3050f834262669b066594e44a
hoda <h.ryosuke820@gmail.com>
2025-01-13 12:52:49 +0900

    UserAvatarにリンクを設定

--- src/app/components/layout/header/Header.tsx
+++ src/app/components/layout/header/Header.tsx
@@ -4,7 +4,9 @@ export const Header = () => {
   return (
     <header className="h-16 px-5 flex items-center justify-between">
       <h1 className="text-2xl font-bold text-title">Hody</h1>
-      <UserAvatar />
+      <a href="https://github.com/dahoho" target="_blank" rel="noreferrer">
+        <UserAvatar />
+      </a>
     </header>
   )
 }
