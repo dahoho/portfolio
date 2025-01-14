41de1a4536de7d51ebb5988b94239836e88b7c33
hoda <h.ryosuke820@gmail.com>
2025-01-14 22:54:02 +0900

    スタイル調整

--- src/app/components/blog/Blog.tsx
+++ src/app/components/blog/Blog.tsx
@@ -2,6 +2,7 @@ import { InnerLayout } from '@/app/components/layout/inner'
 import { Section } from '@/app/components/layout/section'
 import { HeadingLarge } from '@/app/components/ui/heading'
 import dayjs from 'dayjs'
+import Image from 'next/image'
 
 type ZennArticleType = {
   id: number
@@ -35,17 +36,17 @@ export const Blog = async () => {
                   rel="noreferrer"
                   className="block p-2"
                 >
+                  <div className="flex gap-2">
+                    <span>{article.emoji}</span>
+                    <p className="font-bold">{article.title}</p>
+                  </div>
                   <time
                     dateTime={dayjs(article.published_at).format('YYYY-MM-DD')}
-                    className="text-xs"
+                    className="text-xs mt-2 flex gap-1 items-center"
                   >
+                    <Image src="zenn.svg" alt="Zenn" width={12} height={12} />
                     {dayjs(article.published_at).format('YYYY-MM-DD')}
                   </time>
-                  <div className="flex gap-2 mt-1">
-                    <span>{article.emoji}</span>
-
-                    <p>{article.title}</p>
-                  </div>
                 </a>
               </li>
             )
--- src/app/components/ui/heading/HeadingLarge.tsx
+++ src/app/components/ui/heading/HeadingLarge.tsx
@@ -1,5 +1,9 @@
 import { ChildrenType } from '@/app/types/children'
 
 export const HeadingLarge = ({ children }: ChildrenType) => {
-  return <h2 className="text-3xl text-text dark:text-textDark">{children}</h2>
+  return (
+    <h2 className="text-3xl font-bold text-text dark:text-textDark">
+      {children}
+    </h2>
+  )
 }
