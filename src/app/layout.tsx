95a94ee4a37ee7ccbf393c281d45a58d759a12d8
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-01-16 22:03:04 +0900

    Google Analyticsコンポーネントを追加し、ページビューをトラッキングする機能を実装。package.jsonに@types/gtag.jsを追加。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import '@mantine/core/styles.css'
-import type { Metadata } from 'next'
-
-import './styles/globals.css'
-
 import { AppMantineProvider } from '@/app/lib/mantine'
 import { AppThemeProvider } from '@/app/lib/themeProvider'
+
+import GoogleAnalytics from '@/app/components/GoogleAnalytics/GoogleAnalytics'
 import { ColorSchemeScript, mantineHtmlProps } from '@mantine/core'
+import '@mantine/core/styles.css'
+import type { Metadata } from 'next'
+import './styles/globals.css'
 
 export const metadata: Metadata = {
   title: 'portfolio',
@@ -17,9 +17,12 @@ const RootLayout = ({
 }: Readonly<{
   children: React.ReactNode
 }>) => {
+  const gaId = process.env.GA_ID || ''
+
   return (
     <html lang="ja" {...mantineHtmlProps}>
       <head>
+        <GoogleAnalytics />
         <ColorSchemeScript />
       </head>
       <body className="bg-background dark:bg-backgroundDark text-text dark:text-textDark text-sm">
---
+++ src/app/lib/gtag/index.ts
@@ -0,0 +1,16 @@
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    gtag: (...args: any[]) => void
+  }
+}
+
+export const GA_TAG_ID = process.env.NEXT_PUBLIC_GA_ID || ''
+
+export const IS_GATAG = GA_TAG_ID !== ''
+
+export const pageview = (path: string) => {
+  window.gtag('config', GA_TAG_ID, {
+    page_path: path,
+  })
+}
