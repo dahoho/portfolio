f5c209b68e3726007aa0e8348f2d243702019b7b
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-01-18 00:44:22 +0900

    Noto Sans JPフォントを追加し、全体のスタイルに適用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,14 @@ import GoogleAnalytics from '@/app/components/GoogleAnalytics/GoogleAnalytics'
 import { ColorSchemeScript, mantineHtmlProps } from '@mantine/core'
 import '@mantine/core/styles.css'
 import type { Metadata } from 'next'
+import { Noto_Sans_JP } from 'next/font/google'
 import { Suspense } from 'react'
 import './styles/globals.css'
+
+// Noto Sans JP フォントの設定
+const noto = Noto_Sans_JP({
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: 'portfolio',
@@ -26,7 +32,9 @@ const RootLayout = ({
         </Suspense>
         <ColorSchemeScript />
       </head>
-      <body className="bg-background dark:bg-backgroundDark text-text dark:text-textDark text-sm">
+      <body
+        className={`${noto.className} bg-background dark:bg-backgroundDark text-text dark:text-textDark text-sm`}
+      >
         <AppThemeProvider>
           <AppMantineProvider>{children}</AppMantineProvider>
         </AppThemeProvider>
