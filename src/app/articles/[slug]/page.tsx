2e93eb0951bea4b3276672d427bca805e2ea9ef2
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-01-18 00:24:32 +0900

    Articleコンポーネントのスタイル調整

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -37,7 +37,7 @@ export default async function Article({ params }: ParamsType) {
 
   return (
     <ContainerLayout>
-      <div className="w-full h-40 sm:h-64 relative">
+      <div className="w-full h-40 sm:h-64 relative -z-10">
         <Image
           src={article.coverImage.src}
           alt={article.coverImage.altText}
@@ -66,7 +66,10 @@ export default async function Article({ params }: ParamsType) {
             {article.bookUrl}
           </a>
         </p>
-        <div className="prose mt-12 prose-h1:dark:text-textDark prose-h2:dark:text-textDark prose-h1:text-text prose-h2:text-text prose-h1:text-xl prose-h2:text-lg dark:text-textDark text-text prose-h3:dark:text-textDark prose-h3:text-text  prose-h3:text-base pose-h3:font-bold">
+        <div
+          className="prose mt-12 prose-h2:dark:text-textDark
+        prose-h2:text-text prose-h2:text-xl prose-h2:border-b prose-h2:border-border prose-h2:pb-3 prose-h3:dark:text-textDark prose-h3:text-text prose-h3:text-base prose-h3:mt-8 prose-h3:leading-7 prose-h4:dark:text-textDark prose-h4:text-text  prose-h4:text-base pose-h4:font-bold dark:text-textDark text-text text-sm leading-6"
+        >
           <div dangerouslySetInnerHTML={{ __html: article.body }} />
         </div>
       </div>
