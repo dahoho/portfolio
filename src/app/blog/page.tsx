c387b858eb580c06b93f9d4a66e4ce0bfd7ff7c7
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-01-24 23:43:42 +0900

    ブログ記事をコンテナレイアウトでラップ

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,15 @@
 import { Blog } from '@/app/components/blog'
+import { ContainerLayout } from '@/app/components/layout/containerLayout'
 import { getBlogArticles } from '@/app/lib/newt/Blog'
 
 const bookArticles = await getBlogArticles()
 
 const BlogArticles = () => {
-  return <Blog blogArticles={bookArticles} />
+  return (
+    <ContainerLayout>
+      <Blog blogArticles={bookArticles} />
+    </ContainerLayout>
+  )
 }
 
 export default BlogArticles
