e48a406db5443050a6f47e5389b9cccf329c864b
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-01-25 15:35:59 +0900

    OG画像の背景を削除し、タイトルの下に新しいテキストスタイルを追加

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -16,8 +16,6 @@ export const GET = (req: NextRequest) => {
       (
         <div
           style={{
-            backgroundImage:
-              'url(https://portfolio-ryosuke.assets.newt.so/v1/8e7ec721-15ab-4c9e-a8da-001891a8198f/01.webp)',
             backgroundColor: '#fff',
             backgroundSize: '100% 100%',
             height: '100%',
@@ -44,6 +42,19 @@ export const GET = (req: NextRequest) => {
             }}
           >
             {title}
+          </div>
+          <div
+            style={{
+              width: '100%',
+              fontSize: 40,
+              fontStyle: 'normal',
+              fontWeight: 'bold',
+              color: '#000',
+              padding: '0 120px',
+              lineHeight: 1.3,
+            }}
+          >
+            ✏️ Hody
           </div>
         </div>
       ),
