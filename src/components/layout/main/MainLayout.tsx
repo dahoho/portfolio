807bc01c1bae841669d376dee198858ee51d83ed
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-01-28 22:28:55 +0900

    MainLayoutのマージンを調整し、上下のスペースを増加

--- src/components/layout/main/MainLayout.tsx
+++ src/components/layout/main/MainLayout.tsx
@@ -2,6 +2,6 @@ import { ChildrenType } from '@/types/children'
 
 export const MainLayout = ({ children }: ChildrenType) => {
   return (
-    <main className="px-5 my-12 max-w-[1024px] w-full mx-auto">{children}</main>
+    <main className="px-5 my-16 max-w-[1024px] w-full mx-auto">{children}</main>
   )
 }
