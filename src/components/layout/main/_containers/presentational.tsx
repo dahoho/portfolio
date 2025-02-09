a7702e87b97c5e4eba1d9db7ad9a6b0d431f0f5f
Ryosuke Hoda <h.ryosuke820@gmail.com>
2025-02-09 19:40:23 +0900

    メインレイアウトの最大幅を1024pxから980pxに変更

--- src/components/layout/main/_containers/presentational.tsx
+++ src/components/layout/main/_containers/presentational.tsx
@@ -2,7 +2,7 @@ import { ChildrenType } from '@/types/children'
 
 const MainLayoutPresentational = ({ children }: ChildrenType) => {
   return (
-    <main className="px-5 my-16 max-w-[1024px] w-full mx-auto">{children}</main>
+    <main className="px-5 my-16 max-w-[980px] w-full mx-auto">{children}</main>
   )
 }
 
